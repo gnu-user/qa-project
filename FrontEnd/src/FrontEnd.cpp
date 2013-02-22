c6f1a0d24d369d63ae2b313149d7678c513814f1
gnu_user <gsoc.student@gmail.com>
2013-02-22 00:10:22 -0500

    Added a POCO library test to the front end

--- FrontEnd/src/FrontEnd.cpp
+++ FrontEnd/src/FrontEnd.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include <string>
+#include <stdio.h>
+#include <Poco/String.h>
+
+using Poco::trim;
+using Poco::trimLeftInPlace;
+using Poco::trimRight;
+using Poco::trimRightInPlace;
 
 using namespace std;
 
 int main()
 {
+	string hello("  Hello, world!  ");
+	string s1(trimLeftInPlace(hello)); // "Hello, world!  "
+	trimRightInPlace(s1);            // "Hello, world!"
+	string s2(trim(hello));     // "Hello, world!"
+
 	cout << "Hello, World";
 
 	return 0;
