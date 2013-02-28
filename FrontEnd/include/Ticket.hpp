436c9d67ddce70d29306b12b5c1c1257c07474e3
Daniel Smullen <dsmullen@outbreaklabs.com>
2013-02-28 12:43:43 -0500

    added comments

--- FrontEnd/include/Ticket.hpp
+++ FrontEnd/include/Ticket.hpp
@@ -6,24 +6,59 @@
 
 using namespace std;
 
+/**
+ * Ticket Class
+ *
+ * Used for storing ticket data, and provides functions for interacting with them.
+ */
 class Ticket
 {
 
 private:
+    /**
+     * event The event title for the tickets.
+     * seller The seller of the tickets.
+     * volume The amount of tickets available.
+     * price The cost of the tickets in dollars.
+     */
     string event;
     string seller;
     int volume;
     double price;
 
 public:
+    /**
+     * Ticket The constructor for the class, requires all class attributes
+     * to be defined.
+     * @param event The event title for the tickets.
+     * @param seller The seller of the tickets.
+     * @param volume The amount of tickets available.
+     * @param price The cost of the tickets in dollars.
+     */
     Ticket(string event, string seller, int volume, double price);
 
+    /**
+     * get_event Access function for the event title.
+     * @return Returns the event title as a string.
+     */
     string get_event();
    
+    /**
+     * get_seller Access function for the seller.
+     * @return Returns the seller as a string.
+     */
     string get_seller();
    
+    /**
+     * get_volume Access function for the ticket volume.
+     * @return Returns the amount of tickets available as an integer.
+     */
     int get_volume();
    
+    /**
+     * get_price Access function for the ticket price.
+     * @return Returns the cost of the tickets, in dollars.
+     */
     double get_price();
 };
 #endif /* TICKET_HPP_ */
--- FrontEnd/include/Transaction.hpp
+++ FrontEnd/include/Transaction.hpp
@@ -7,23 +7,73 @@
 
 using namespace std;
 
+/**
+ * Transaction Class
+ *
+ * Acts as a superclass for the other transaction classes.
+ */
 class Transaction
 {
 
 protected:
+    /**
+     * user Stores the user who performed the transaction.
+     * code Stores the transaction code.
+     * transaction Stores the formatted transaction string.
+     */
     User user;
     string code;
     string transaction;
    
+    /**
+     * save_transaction Virtual function signature for
+     * class abstraction in C++.
+     */
     virtual void save_transaction();
 
     /* Transaction formatting helpers, overloads for each type of transaction format */
     // TODO fix this, have two overloads that are the same... not possible
+    /**
+     * format Formats transactions which require the usage of a user name, type,
+     * and credit amount.
+     * @param code The transaction code for the transaction.
+     * @param username The username specified for the transaction.
+     * @param type The type of user for the transaction.
+     * @param credit The credit amount for the transaction.
+     * @return Returns a formatted transaction string containing all arguments.
+     */
     string format(string code, string username, string type, double credit);
+
+    /**
+     * format Formats transactions which require the usage of a user name, type,
+     * seller, and credit amount.
+     * @param code The transaction code for the transaction.
+     * @param username The user name specified for the transaction.
+     * @param type The type of user for the transaction.
+     * @param seller The seller user name specified for the transaction.
+     * @param refund The credit amount for the transaction.
+     * @return Returns a formatted transaction string containing all arguments.
+     */
     string format(string code, string username, string type, string seller, double refund);
+
+    /**
+     * format Formats transactions which require the usage of an event title,
+     * seller user name, ticket volume, and price.
+     * @param code The transaction code for the transaction.
+     * @param event The event title specified for the transaction.
+     * @param seller The seller user name specified for the transaction.
+     * @param volume The number of tickets in the transaction.
+     * @param price The price of the tickets in the transaction, in dollars.
+     * @return Returns a formatted transaction string containing all arguments.
+     */
     string format(string code, string event, string seller, int volume, double price);
 
 public:
+    /**
+     * get_transaction Provides the stored formatted transaction string
+     * for any transaction.
+     * @return Returns the formatted transaction string.
+     */
     string get_transaction();
 };
 #endif /* TRANSACTION_HPP_ */
