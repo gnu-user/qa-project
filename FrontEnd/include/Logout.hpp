2f30a25d85bbc5229755b52881835a709008ef70
Daniel Smullen <dsmullen@outbreaklabs.com>
2013-02-28 12:09:22 -0500

    added comments

--- FrontEnd/include/Logout.hpp
+++ FrontEnd/include/Logout.hpp
@@ -5,12 +5,27 @@
 #include "Exception.hpp"
 #include "User.hpp"
 
+/**
+ * Logout Class
+ *
+ * Used for the logout transaction functions and attributes.
+ */
 class Logout : public Transaction
 {
 protected:
+    /**
+     * save_transaction Virtual function signature for
+     * class abstraction in C++.
+     */
     virtual void save_transaction();
 
 public:
+    /**
+     * Logout The constructor for the class, requires a handle
+     * to the current user logged in.
+     * @param current_user The current user logged in, to be
+     * logged out.
+     */
     Logout(User current_user);
 };
 #endif /* LOGOUT_HPP_ */
