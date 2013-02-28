c215ba37ecd8e4151fc37795185629dd245178bf
Daniel Smullen <dsmullen@outbreaklabs.com>
2013-02-28 00:07:24 -0500

    updated comments

--- FrontEnd/include/Delete.hpp
+++ FrontEnd/include/Delete.hpp
@@ -9,14 +9,34 @@
 
 using namespace std;
 
+/**
+ * Delete Class
+ *
+ * Used for the delete transaction functions and attributes.
+ */
 class Delete : public Transaction
 {
 protected:
+	/**
+	 * save_transaction Virtual function signature used for
+	 * interface creation in C++.
+	 */
     virtual void save_transaction();
 
 public:
+	/**
+	 * Delete The constructor for the class,
+	 * requires a reference to the current user session.
+	 * @param current_user
+	 */
     Delete(User current_user);
    
+    /**
+     * process_username Validates and processes a username for
+     * deletion.
+     * @param input The username to delete.
+     * @param user_accounts A handle to the current user accounts.
+     */
     void process_username(string input, CurrentUserAccounts user_accounts);
 };
 #endif /* DELETE_HPP_ */
