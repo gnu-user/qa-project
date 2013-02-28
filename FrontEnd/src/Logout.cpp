c18998f28b111321de6b5b1214e1043a21454f39
gnu_user <gsoc.student@gmail.com>
2013-02-27 20:21:27 -0500

    Finished the logout transaction

--- FrontEnd/src/Logout.cpp
+++ FrontEnd/src/Logout.cpp
@@ -8,10 +8,17 @@ Logout::Logout(User current_user)
         throw Exception(MUST_LOGIN);
     }
 
+    this->code = "00";
     this->user = current_user;
+    save_transaction();
 }
 
 void Logout::save_transaction()
 {
-    throw Exception(NOT_YET_IMPLEMENTED);
+    this->transaction = format(
+                            code,
+                            user.get_username(),
+                            user.get_type(),
+                            user.get_credit()
+                        );
 }
