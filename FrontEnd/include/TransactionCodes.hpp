563f22f06568bfa7e47cc8bba128b200b3b603e0
Daniel Smullen <dsmullen@outbreaklabs.com>
2013-02-28 13:07:07 -0500

    added comments

--- FrontEnd/include/TransactionCodes.hpp
+++ FrontEnd/include/TransactionCodes.hpp
@@ -12,20 +12,24 @@
 
 using namespace std;
 
-/* Enumeration of the available front end transactions codes */
+/**
+ * Transaction codes used for the transactions supported.
+ */
 enum transaction_codes {
-            _undefined,
-            _login,
-            _logout,
-            _create,
-            _delete,
-            _sell,
-            _buy,
-            _refund,
-            _addcredit
+            _undefined,//!< _undefined Used for undefined transactions.
+            _login,    //!< _login Used for the login transaction.
+            _logout,   //!< _logout Used for the logout transaction.
+            _create,   //!< _create Used for the create transaction.
+            _delete,   //!< _delete Used for the delete transaction.
+            _sell,     //!< _sell Used for the sell transaction.
+            _buy,      //!< _buy Used for the buy transaction.
+            _refund,   //!< _refund Used for the refund transaction.
+            _addcredit //!< _addcredit used for the add credit transaction.
 };
 
-/* Mapping of each transaction as a string to the transaction code */
+/**
+ * map_code Maps each transaction code to a transaction string.
+ */
 static map<string, transaction_codes> map_code = {
             {"login", _login},
             {"logout", _logout},
