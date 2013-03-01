8a34344b719511755aadf279285bf817af52629f
Daniel Smullen <dsmullen@outbreaklabs.com>
2013-03-01 16:17:19 -0500

    Added the Refund transaction

--- FrontEnd/src/Refund.cpp
+++ FrontEnd/src/Refund.cpp
@@ -1,26 +1,118 @@
 #include "../include/Refund.hpp"
+#include "../include/Validate.hpp"
 
 Refund::Refund(User current_user)
 {
-    throw Exception(NOT_YET_IMPLEMENTED);
+    /* If the user has not logged in throw exception */
+    if (!current_user.get_status())
+    {
+        throw Exception(MUST_LOGIN);
+    }
+
+    /* Verify the user has the necessary privileges */
+    if (!current_user.has_permissions("refund"))
+    {
+        throw Exception(INVALID_PRIV);
+    }
+
+    this->code = "05";
+    this->user = current_user;
 }
 
 void Refund::process_buyer(string buyer, CurrentUserAccounts user_accounts)
 {
-    throw Exception(NOT_YET_IMPLEMENTED);
+    try
+    {
+        /* Attempt to validate the username, throw exception if invalid */
+        if (Validate::username(buyer))
+        {
+            /* Check that the buyer exists */
+            if (user_accounts.has_user(buyer))
+            {
+                /* Get the buyer for the refund */
+                this->buyer = buyer;
+            }
+            else
+            {
+                throw Exception(UNKNOWN_USER);
+            }
+        }
+        else
+        {
+            throw Exception(INVALID_USER);
+        }
+    }
+    /* Catch any exceptions and throw them to the calling function */
+    catch (Exception& e)
+    {
+        throw e;
+    }
 }
 
 void Refund::process_seller(string seller, AvailableTickets available_tickets)
 {
-    throw Exception(NOT_YET_IMPLEMENTED);
+    try
+    {
+        /* Attempt to validate the username, throw exception if invalid */
+        if (Validate::username(seller))
+        {
+            /* Check that the seller exists */
+            if (user_accounts.has_user(seller))
+            {
+                /* Get the seller for the refund */
+                this->seller = seller;
+            }
+            else
+            {
+                throw Exception(UNKNOWN_USER);
+            }
+        }
+        else
+        {
+            throw Exception(INVALID_USER);
+        }
+    }
+    /* Catch any exceptions and throw them to the calling function */
+    catch (Exception& e)
+    {
+        throw e;
+    }
 }
 
 void Refund::process_credit(string credit)
 {
-    throw Exception(NOT_YET_IMPLEMENTED);
+    double tmp_credit = 0.0;
+
+        try
+        {
+            /* Validate the credit is in dollars amount */
+            if (Validate::dollars(credit, tmp_credit))
+            {
+                /* Verify credit is less than overflow, 999999.99 */
+                if (credit <= 999999.99)
+                {
+                    this->credit = tmp_credit;
+                }
+                else
+                {
+                    throw Exception(CREDIT_AMOUNT_OVERFLOW);
+                }
+            }
+        }
+        /* Catch any exceptions and throw them to the calling function */
+        catch (Exception& e)
+        {
+            throw e;
+        }
 }
 
 void Refund::save_transaction()
 {
-    throw Exception(NOT_YET_IMPLEMENTED);
+    this->transaction = format(
+                            code,
+                            buyer,
+                            user.get_type(),
+                            seller,
+                            credit
+                        );
 }
