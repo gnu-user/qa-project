c7704257ff785e9cad74dd505118720d680d1301
gnu_user <gsoc.student@gmail.com>
2013-03-15 23:58:45 -0400

    Finished the test framework, test cases just need to be updated
    The test framework is finished but needs to be cleaned up and have
    proper RDOc documentation added.

--- test-framework/TestFramework.rb
+++ test-framework/TestFramework.rb
@@ -21,9 +21,9 @@
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ###############################################################################
-require 'TestSuite'
-require 'TestCase'
-require 'colorize'
+require './TestSuite'
+require './TestCase'
+require './colorize'
 
 
 # Simple function which verifies if a test case or test suite
@@ -31,13 +31,34 @@ require 'colorize'
 # has been executed, but has not passed
 def unmet_dependencies?(test, test_history)
 	test.dependencies.each() do |dependency|
-		# If the test dependency has been executed and failed return false
-		if test_history.has_key?(dependency) and test_history[dependency] != "Passed"
+		# If the test dependency has been executed and failed return true
+		if test_history.has_key?(dependency) and test_history[dependency].status != "Passed"
 			return true
 		end
 	end
 
 	return false
+end
+
+
+# Yields a list of the unmet dependencies, these are dependencies for the test case
+# or test suite that have failed.
+def failed_dependencies(test, test_history)
+	test.dependencies.each() do |dependency|
+		# Yield each test dependency that failed
+		if test_history.has_key?(dependency) and test_history[dependency].status != "Passed"
+			yield test_history[dependency] 
+		end
+	end
+end
+
+
+# Diffs the two files specified and saves the results to a .diff file in the same
+# directory as the second file that is being compared
+def diff_files(file_1, file_2)
+	output_file = file_2.sub(/\.test$/, '')
+	system("diff -scbBa #{file_1} #{file_2} " +
+	   	   "| sed -e 's/\\\\ No newline at end of file$//g' > #{output_file}.diff")
 end
 
 
@@ -66,9 +87,6 @@ tests_path = ARGV[1]
 output_path = ARGV[2]
 
 
-
-
-
 # Parse each test suite and all test cases and store the objects
 # in the master test list array
 if File.exists?("#{tests_path}/master.lst")
@@ -88,32 +106,131 @@ else
 	raise "#{tests_path}/master.lst not found!"
 end
 
-# For each test suite verify that the test case dependencies have
+
+# For each test suite verify that the test suite dependencies have
 # been met then execute each of the test cases
 master_testlist.each() do |test_suite|
-	puts "Executing #{test_suite.title}"
-	puts "=" * 40 + "\n"
-	# If the test suite has test suite dependencies that have not 
-	# been met, then the test suite cannot be executed
+	puts "\n\nExecuting #{test_suite.title}"
+	printf "%-40s\n", "=" * 40
+
+	# The path to the test case and results
+	test_path = "#{tests_path}/#{test_suite.name}"
+	result_path = "#{output_path}/#{test_suite.name}"
+
+	# Add each test suite executed to the test history
+	test_history[test_suite.name] = test_suite
+	
+	# If test suite dependencies that have not been met, test suite cannot be executed
 	if unmet_dependencies?(test_suite, test_history)
-		# Test suite failed, unmet dependencies
-		puts "#{test_suite.title} Failed due to the following unmet dependencies".red
-		test_suite.dependencies.each() do |dependency|
-			puts "#{dependency}".light_blue
+
+		test_suite.set_status("Unmet Dependencies")
+		printf "%-70s %s\n", test_suite.title, "FAILED".red
+		puts "Failed due to the following unmet dependencies:"
+		
+		failed_dependencies(test_suite, test_history) do |dependency|
+			puts "#{dependency.title}".yellow
+		end
+
+		# Display the horizontal bar at bottom of test suite results
+		printf "%-40s %s %s\n", "-" * 40, " " * 28, "-" * 8
+		printf "%-70s %s\n", test_suite.title, "FAILED".red
+
+		next
+	end
+
+
+	# Create a corresponding output directory for test suite results
+	unless File.directory?(result_path)
+		Dir.mkdir(result_path)
+	end
+
+
+	# Execute each test case and save the console output and daily transaction file
+	test_suite.test_cases.each() do |test_case|
+
+		# Path to the test case input directory and test case
+		test_file = "#{test_path}/#{test_case.name}"
+		result_file = "#{result_path}/#{test_case.name}"
+		out_status = 0
+		dtf_status = 0
+
+		# Add each test case executed to the test history
+		test_history[test_case.name] = test_case
+
+		# If test case dependencies that have not been met, test suite cannot be executed
+		if unmet_dependencies?(test_case, test_history)
+			
+			test_case.set_status("Unmet Dependencies")
+
+			printf "%-70s %s\n", test_case.title, "FAILED".red
+			puts "Failed due to the following unmet dependencies:"
+			
+			failed_dependencies(test_case, test_history) do |dependency|
+				puts "#{dependency.title}".yellow
+			end
+
+			next
+		end
+
+		# Execute the test case and save the console output
+		system("#{binary_path} #{test_file}.cua #{test_file}.atf #{result_file}.dtf.test " +
+			   " < #{test_file}.inp &> #{result_file}.out.test")
+
+		# Diff the expected console output and the daily transaction file
+		# if there are any differences the test case has failed
+		system("diff -sq #{test_file}.out #{result_file}.out.test > /dev/null")
+		out_status = $?.exitstatus
+
+		# Diff the daily transaction files if one was created
+		unless File.zero?("#{test_file}.dtf")
+			system("diff -sq #{test_file}.dtf #{result_file}.dtf.test > /dev/null")
+			dtf_status = $?.exitstatus
+		end
+
+		# The test case failed if console output or daily transaction files differ
+		if out_status != 0 or dtf_status != 0
+			# Display the results of the test case, set test case status
+			printf "%-70s %s\n", test_case.title, "FAILED".red
+			test_case.set_status("Failed")
+
+			# If the daily transaction files differ save the diff output
+			if dtf_status != 0
+				diff_files("#{test_file}.dtf", "#{result_file}.dtf.test")
+			end
+
+			# If the console output differs save the diff output
+			if out_status != 0
+				diff_files("#{test_file}.out", "#{result_file}.out.test")
+			end
+		else
+			# Display the results of the test case, set test case status
+			printf "%-70s %s\n", test_case.title, "PASSED".green
+			test_case.set_status("Passed")
 		end
 	end
 
-	puts "-----------------------------------------------"
-	puts test_suite.name
-	puts test_suite.title
-	puts test_suite.description
-	puts test_suite.dependencies
-	puts "\n"
-	
+	# Display the summary of pass or fail for each test suite
 	test_suite.test_cases.each() do |test_case|
-		puts test_case.name
-		puts test_case.title
-		puts test_case.description + "\n"
+		if test_case.status != "Passed"
+			test_suite.set_status("Failed")
+			break
+		end
 	end
-	puts "\n\n" 
+
+	# Display the horizontal bar at bottom of test suite results
+	printf "%-40s %s %s\n", "-" * 40, " " * 28, "-" * 8
+
+	if test_suite.status != "Failed" and test_suite.status != "Unmet Dependencies"
+		test_suite.set_status("Passed")
+		printf "%-70s %s\n", test_suite.title, "PASSED".green
+	else
+		printf "%-70s %s\n", test_suite.title, "FAILED".red
+	end
 end
+
+
+#test_history.each() do |key, value|
+#	puts key
+#	puts value.title
+#	puts value.status, "\n"
+#end
