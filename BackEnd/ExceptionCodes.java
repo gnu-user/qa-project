9269d24d9d255f213b6b1377b33ecae7560c442d
gnu_user <gsoc.student@gmail.com>
2013-03-24 13:13:15 -0400

    Added the exception codes enumeration

--- BackEnd/ExceptionCodes.java
+++ BackEnd/ExceptionCodes.java
@@ -20,5 +20,57 @@
  */
 public enum ExceptionCodes
 {
+    /* Fatal error exception codes */
+    ATF_NOT_FOUND("ATF_NOT_FOUND", "Available tickets file not found, check arguments provided."),
+    CUA_NOT_FOUND("CUA_NOT_FOUND", "Current user accounts file not found, check arguments provided."),
+    DTF_NOT_FOUND("DTF_NOT_FOUND", "Daily transactions file not found, check arguments provided."),
+    CORRUPT_ATF("CORRUPT_ATF", "An error occurred reading the available tickets file, data corrupted."),
+    CORRUPT_CUA("CORRUPT_CUA", "An error occurred reading the current user account file, data corrupted."),
+    CORRUPT_DTF("CORRUPT_DTF", "An error occurred reading the daily transactions file, data corrupted."),
+    ATF_WRITE_ERROR("ATF_WRITE_ERROR", "An error occurred writing the available tickets file."),
+    CUA_WRITE_ERROR("CUA_WRITE_ERROR", "An error occurred writing the current user accounts file."),
+    
+    /* Failed constraints exception codes */
+    INVALID_USER_EXISTS("INVALID_USER_EXISTS", "Invalid username, user already exists."),
+    UNKNOWN_USER("UNKNOWN_USER", "Invalid username, username does not exist."),
+    USER_CREDIT_NEGATIVE("USER_CREDIT_NEGATIVE", "Invalid credit amount, user credit cannot be less than 0.00."),
+    USER_CREDIT_OVERFLOW("USER_CREDIT_OVERFLOW", "Invalid credit amount, user credit cannot exceed 999999.99."),
+    TICKET_VOLUME_NEGATIVE("TICKET_VOLUME_NEGATIVE", "Invalid number of tickets, volume cannot be less than 0."),
+    TICKET_VOLUME_OVERFLOW("TICKET_VOLUME_OVERFLOW", "Invalid number of tickets, volume cannot exceed 100."),
+    
+    NOT_YET_IMPLEMENTED("NOT_YET_IMPLEMENTED", "Not yet implemented");
+    
+    private String type;
+    private String description;
+    
+    private ExceptionCodes(String type, String description)
+    {
+        this.type = type;
+        this.description = description;
+    }
 
+    /**
+     * @return the type
+     */
+    public String getType()
+    {
+        return type;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription()
+    {
+        return description;
+    }
+    
+    /**
+     * Create a string representation of the exception
+     */
+    @Override
+    public String toString()
+    {
+        return type + ": " + description;
+    }
 }
