31d53c657c5467729c2f937487889c551e62a5d5
gnu_user <gsoc.student@gmail.com>
2013-03-25 00:06:41 -0400

    Added support for parsing the merged daily transaction files

--- BackEnd/DailyTransactions.java
+++ BackEnd/DailyTransactions.java
@@ -24,12 +24,15 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class DailyTransactions
 {
     private ArrayList<String> mergedTransactions;
-    private LinkedHashMap<Transaction, String> transactions;
+    private ArrayList<Transaction> transactions;
     private String dtfDirectory;
 
 
@@ -46,13 +49,13 @@ public class DailyTransactions
         {
             this.dtfDirectory = dtfDirectory;
             this.mergedTransactions = new ArrayList<String>();
-            this.transactions = new LinkedHashMap<Transaction, String>();
+            this.transactions = new ArrayList<Transaction>();
             
             /* Merge the daily transaction files */
             merge();
             
             /* Parse the merged transactions */
-            //parse();
+            parse();
         }
         else
         {
@@ -60,17 +63,33 @@ public class DailyTransactions
         }
     }
 
-    public LinkedHashMap<Transaction, String> getTransactions()
+    public ArrayList<Transaction> getTransactions()
     {
         return transactions;
     }
     
     public void displayTransactions()
     {
-        for (String transaction : mergedTransactions)
+        for (Transaction transaction : transactions)
         {
-            System.out.println(transaction);
+            System.out.println(transaction.getTransaction());
         }
+    }
+    
+    /**
+     * Sorts an array of files by date modified in descending order,
+     * the oldest modified file is the first in the resulting array.
+     * 
+     * @param files
+     * @return
+     */
+    private void sortModified(File[] files)
+    {
+        Arrays.sort(files, new Comparator<File>(){
+            public int compare(File file1, File file2)
+            {
+                return Long.valueOf(file1.lastModified()).compareTo(file2.lastModified());
+            } }); 
     }
     
     /**
@@ -83,9 +102,13 @@ public class DailyTransactions
     {
         BufferedReader reader;
         String line;
+        File[] files = new File(dtfDirectory).listFiles();
+        
+        /* Sort the files by modified date, the older modified file is processed first */
+        sortModified(files);
         
         /* Merge each of the daily transaction files */
-        for (File file : new File(dtfDirectory).listFiles())
+        for (File file : files)
         {
             if (file.getName().matches("^.+\\.dtf$"))
             {
@@ -139,9 +162,122 @@ public class DailyTransactions
         }
     }
 
-    private void parse()
+    /**
+     * @throws FatalError
+     */
+    private void parse() throws FatalError
     {
-        throw new UnsupportedOperationException();
-    }
+        /* Get the transaction code */
+        Pattern reCode = Pattern.compile("^([0-9]{2})_.*$");
+        
+        /* Daily transaction entry matches logout, create, delete, or addcredit */
+        Pattern reUserMod = Pattern.compile("^(00|01|02|06)_([A-Za-z0-9_]{1,15}?)_+(AA|FS|BS|SS)_([0-9]{6}\\.[0-9]{2})$");
 
+        /* Daily transaction entry matches refund */
+        Pattern reRefund = Pattern.compile("^(05)_([A-Za-z0-9_]{1,15}?)_+([A-Za-z0-9_]{1,15}?)_+([0-9]{6}\\.[0-9]{2})$");
+
+        /* Daily transaction entry matches buy or sell */
+        Pattern reBuySell = Pattern.compile("^(03|04)_(.{1,19}?)_+_([A-Za-z0-9_]{1,15}?)_+([0-9]{3})_([0-9]{3}\\.[0-9]{2})$");
+
+        Matcher match;
+        
+        /* Parse each entry in the merged transactions */
+        for (String entry : mergedTransactions)
+        {
+            /* Get the transaction code and create the appropriate object */
+            match = reCode.matcher(entry);
+            
+            /* logout, create, delete, or addcredit transaction */
+            if (match.matches() && match.group(1).matches("(00|01|02|06)"))
+            {
+                match = reUserMod.matcher(entry);
+            
+                if (match.matches())
+                {
+                    String username = match.group(2);
+                    String type = match.group(3);
+                    Double credit = Double.parseDouble(match.group(4));
+                    
+                    /* Add a logout transaction */
+                    if (match.group(1).equals("00"))
+                    {
+                        transactions.add(new Logout(username, type, credit, entry));
+                    }
+                    /* Add a create transaction */
+                    else if (match.group(1).equals("01"))
+                    {
+                        transactions.add(new Create(username, type, credit, entry));
+                    }
+                    /* Add a delete transaction */
+                    else if (match.group(1).equals("02"))
+                    {
+                        transactions.add(new Delete(username, type, credit, entry));
+                    }
+                    /* Add a addcredit transaction */
+                    else if (match.group(1).equals("06"))
+                    {
+                        transactions.add(new AddCredit(username, type, credit, entry));
+                    }
+                }
+                /* This error should never be thrown -- check merge()! */
+                else
+                {
+                    throw new FatalError(ExceptionCodes.CORRUPT_DTF, entry);
+                }
+            }
+            /* Refund transactions */
+            else if (match.group(1).matches("05"))
+            {
+                match = reRefund.matcher(entry);
+                
+                if (match.matches())
+                {
+                    String buyer = match.group(2);
+                    String seller = match.group(3);
+                    Double credit = Double.parseDouble(match.group(4));
+                    
+                    transactions.add(new Refund(buyer, seller, credit, entry));
+                }
+                /* This error should never be thrown -- check merge()! */
+                else
+                {
+                    throw new FatalError(ExceptionCodes.CORRUPT_DTF, entry);
+                }
+            }
+            else if (match.group(1).matches("(03|04)"))
+            {
+                match = reBuySell.matcher(entry);
+                
+                if (match.matches())
+                {
+                    /* Replace each _ in the event title with a space character */
+                    String event = match.group(2).replace('_', ' ');
+                    String seller = match.group(3);
+                    Integer volume = Integer.parseInt(match.group(4));
+                    Double price = Double.parseDouble(match.group(5));
+                    
+                    /* Add a sell transaction */
+                    if (match.group(1).equals("03"))
+                    {
+                        transactions.add(new Sell(event, seller, volume, price, entry));
+                    }
+                    /* Add a buy transaction */
+                    else if (match.group(1).equals("04"))
+                    {
+                        transactions.add(new Buy(event, seller, volume, price, entry));
+                    }
+                }
+                /* This error should never be thrown -- check merge()! */
+                else
+                {
+                    throw new FatalError(ExceptionCodes.CORRUPT_DTF, entry);
+                }
+            }
+            /* This error should never be thrown -- check merge()! */
+            else
+            {
+                throw new FatalError(ExceptionCodes.CORRUPT_DTF, entry);
+            }
+        }
+    }
 }
