cc0719acfd5339417c85a92a712779e73dcf1ae8
Daniel Smullen <dsmullen@outbreaklabs.com>
2013-03-28 21:05:16 -0400

    added javadoc comments

--- BackEnd/src/FailedConstraint.java
+++ BackEnd/src/FailedConstraint.java
@@ -18,17 +18,28 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+/**
+ * FailedConstraint A special type of exception class that allows for the inclusion of
+ * data within the exception itself. This includes error codes and transaction strings
+ * that cause failed constraints within the back-end.
+ * 
+ * @author Jonathan Gillett
+ * @author Daniel Smullen
+ * @author Rayan Alfaheid
+ */
 public class FailedConstraint extends Exception
 {
-    /**
-     * 
-     */
+	//Must be included in exceptions.
     private static final long serialVersionUID = 2801771587969692843L;
 
     /**
+     * The constructor for the class. Takes an exception code and a transaction string
+     * in order to generate a standard exception by calling the superclass constructor
+     * method.
      * 
-     * @param exception
-     * @param transaction
+     * @param exception The ExceptionCodes object containing the exception being thrown.
+     * @param transaction A string containing the transaction string that caused the error.
      */
     public FailedConstraint(ExceptionCodes exception, String transaction)
     {
