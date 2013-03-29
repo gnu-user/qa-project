a2cbb9e61e06f7c9c42a3db9ea95d1c692d81bb5
Daniel Smullen <dsmullen@outbreaklabs.com>
2013-03-28 20:45:39 -0400

    added javadoc comments

--- BackEnd/src/Delete.java
+++ BackEnd/src/Delete.java
@@ -18,6 +18,12 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+
+/**
+ * @author 100440203
+ *
+ */
 public class Delete implements Transaction
 {
     private String username;
@@ -27,10 +33,12 @@ public class Delete implements Transaction
 
 
     /**
-     * 
-     * @param username
-     * @param type
-     * @param credit
+     * Constructor for the class. Accepts and populates class attributes with values passed in.
+     *
+     * @param username String containing the username of the user to delete.
+     * @param type String containing the type of the user to delete.
+     * @param credit Dollar value containing the credit amount of the user to be deleted.
+     * @param transaction String containing the transaction string from the daily transaction file.
      */
     public Delete(String username, String type, Double credit, String transaction)
     {
@@ -40,6 +48,18 @@ public class Delete implements Transaction
         this.transaction = transaction;
     }
 
+    /**
+     * execute Performs the delete transaction. Deletes a user account and updates
+     * the current user accounts file.
+     * 
+     * @param cua A reference to the CurrentUserAccounts object used by the back-end.
+     * @param atf A reference to the AvailableTickets object used by the back-end.
+     * 
+     * @throws FailedConstraint Throws a failed constraint under the following circumstances:
+     * 			<br>If the user name specified does not exist.
+     * 
+     * @see Transaction#execute(CurrentUserAccounts, AvailableTickets)
+     */
     public void execute(CurrentUserAccounts cua, AvailableTickets atf) throws FailedConstraint
     {
         if (cua.hasUser(username))
@@ -52,6 +72,9 @@ public class Delete implements Transaction
         }
     }
 
+	/* (non-Javadoc)
+	 * @see Transaction#getTransaction()
+	 */
 	public String getTransaction() {
 		return transaction;
 	}
