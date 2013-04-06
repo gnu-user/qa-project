fe1a381f0d51a00b82a19ee59f932f354d327bff
gnu_user <gsoc.student@gmail.com>
2013-04-05 20:27:36 -0400

    Refactored the Ticket class test suite

--- BackEnd/test/TicketTest.java
+++ BackEnd/test/TicketTest.java
@@ -1,19 +1,32 @@
+/**
+ * Swift Ticket -- Back End
+ *
+ * Copyright (C) 2013, Jonathan Gillett, Daniel Smullen, and Rayan Alfaheid
+ * All rights reserved.
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
 
-/**
- * 
- */
 
-/**
- * @author jon
- *
- */
 public class TicketTest
 {
-    /* Ticket object user for the tests */
+    /* Ticket object used for the tests */
     private Ticket ticket;
     
     /* Parameters for the Ticket object and used for tests */
