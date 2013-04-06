6621028b17130ef3ff608986448d20a0b26856b1
gnu_user <gsoc.student@gmail.com>
2013-04-05 21:37:20 -0400

    Added a test runner for executing the test suites

---
+++ BackEnd/test/AllTests.java
@@ -0,0 +1,11 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+
+@RunWith(Suite.class)
+@SuiteClasses({TicketTest.class, UserTest.class, ValidateTest.class, CreateTest.class})
+public class AllTests
+{
+
+}
