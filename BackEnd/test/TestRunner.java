ab652c7959ef23a187e4accc327227e8d25785e9
gnu_user <gsoc.student@gmail.com>
2013-04-09 19:49:02 -0400

    Added a test runner for executing the test suite from the command line

---
+++ BackEnd/test/TestRunner.java
@@ -0,0 +1,33 @@
+/**
+ * Swift Ticket -- Back End
+ *
+ * Copyright (C) 2013, Jonathan Gillett, Daniel Smullen, and Rayan Alfaheid
+ * All rights reserved.
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+import org.junit.runner.JUnitCore;
+
+
+/**
+ * The test runner for executing all of the test suites
+ */
+public class TestRunner
+{
+    public static void main(String[] args) throws Exception
+    {
+        JUnitCore.main("AllTests");
+    }
+}
