84249423fd5e9abdf8fdc7d31b6b8d63d1a050ec
gnu_user <gsoc.student@gmail.com>
2013-04-09 01:39:00 -0400

    Added the template for the current user accounts test suite

---
+++ BackEnd/test/CurrentUserAccountsTest.java
@@ -0,0 +1,136 @@
+/**
+ * Swift Ticket -- Back End
+ *
+ * Copyright (C) 2013, Jonathan Gillett, Daniel Smullen, and Rayan Alfaheid
+ * All rights reserved.
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class CurrentUserAccountsTest
+{
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception
+    {
+    }
+
+    /**
+     * Test method for {@link CurrentUserAccounts#addUser(User)}.
+     */
+    @Test
+    public void testAddUser()
+    {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link CurrentUserAccounts#deleteUser(java.lang.String)}.
+     */
+    @Test
+    public void testDeleteUser()
+    {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link CurrentUserAccounts#getAllUsers()}.
+     */
+    @Test
+    public void testGetAllUsers()
+    {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link CurrentUserAccounts#getUser(java.lang.String)}.
+     */
+    @Test
+    public void testGetUser1()
+    {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link CurrentUserAccounts#getUser(java.lang.String)}.
+     */
+    @Test
+    public void testGetUser2()
+    {
+        fail("Not yet implemented"); // TODO
+    }
+    
+    /**
+     * Test method for {@link CurrentUserAccounts#hasUser(java.lang.String)}.
+     */
+    @Test
+    public void testHasUser1()
+    {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link CurrentUserAccounts#hasUser(java.lang.String)}.
+     */
+    @Test
+    public void testHasUser2()
+    {
+        fail("Not yet implemented"); // TODO
+    }
+    
+    /**
+     * Test method for {@link CurrentUserAccounts#write()}.
+     */
+    @Test
+    public void testWrite()
+    {
+        fail("Not yet implemented"); // TODO
+    }
+    
+    /**
+     * Test method for {@link CurrentUserAccounts#parse()}.
+     */
+    @Test
+    public void testParse1()
+    {
+        fail("Not yet implemented"); // TODO
+    }
+    
+    /**
+     * Test method for {@link CurrentUserAccounts#parse()}.
+     */
+    @Test
+    public void testParse2()
+    {
+        fail("Not yet implemented"); // TODO
+    }
+    
+    /**
+     * Test method for {@link CurrentUserAccounts#parse()}.
+     */
+    @Test
+    public void testParse3()
+    {
+        fail("Not yet implemented"); // TODO
+    }
+}
